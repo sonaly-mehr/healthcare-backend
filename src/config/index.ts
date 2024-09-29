aa0f17b341e8b7e3b448786a064ac3231b7f17f3
sonaly-mehr <sonaly.mehr@gmail.com>
2024-09-29 16:18:43 +0600

    updated

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,12 @@
 import dotenv from 'dotenv';
 import path from 'path';
 
-const envPath =
-  process.env.NODE_ENV === 'production'
-    ? path.join(process.cwd(), '.env.prod')
-    : path.join(process.cwd(), '.env');
+// const envPath =
+//   process.env.NODE_ENV === 'production'
+//     ? path.join(process.cwd(), '.env.prod')
+//     : path.join(process.cwd(), '.env');
 
-dotenv.config({ path: envPath });
+dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 export default {
   env: process.env.NODE_ENV,
